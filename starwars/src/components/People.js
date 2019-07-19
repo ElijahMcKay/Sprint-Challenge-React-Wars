7a171a0dc3b7f5861b233340eabd015de489bf29
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-19 13:55:55 -0500

    props added and now working on initial styling

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -29,7 +29,7 @@ function People(props){
             <StyleP>
                 {props.height}
             </StyleP>
-            <StyleP>
+            <StyleP> 
                 {props.hairColor}
             </StyleP>
             <StyleP>
