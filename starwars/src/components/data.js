2f165067ee096895ef1ddec0fdeb027cc60fac1b
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-19 13:38:32 -0500

    completed props and working on styling

---
+++ starwars/src/components/data.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react"; 
+import axios from "axios"; 
+import People from "./People"; 
+import styled from "styled-components"; 
+
+
+function Data() {
+    const [data, setData] = useState([]); 
+
+
+    useEffect(() => {
+        axios
+        .get('https://swapi.co/api/people/')
+        .then(response => {
+            // console.log(`${response.data.results}`)
+            setData(response.data.results); 
+        })
+        .catch(error => {
+            console.log('There was an error:', error); 
+        })}, []); 
+        // console.log(data); 
+        
+        // const StyleDiv = styled.div`
+        // text-align: center
+
+        return (
+            <div>
+                {data.map(obj => (
+                    <People 
+                    name={obj.name}
+                    height={obj.height}
+                    hairColor={obj.hair_color}
+                    gender={obj.gender}
+                    key={obj.url}
+                    /> 
+                    ))}
+                
+                {/* <Image imageSrc={data.url}/>    
+                <Title title={data.title}/>
+                <Date date={data.date}/>
+                <Explanation exp={data.explanation}/>
+                <Copyright copyright={data.copyright}/> */}
+            </div>
+        )
+
+}
+
+export default Data; 
+
+// function Data() {
+//     const [data, setData] = useState([]); 
+
+
+//     useEffect(() => {
+//         axios
+//         .get('https://swapi.co/api/people/')
+//         .then(response => {
+//             // console.log(`${response.data.results}`)
+//             setData(response.data.results); 
+//         })
+//         .catch(error => {
+//             console.log('There was an error:', error); 
+//         })}, [data]); 
+//         console.log(data); 
+//         return (
+//             <div>
+//                 <People>{data.map(obj => {
+//                     return <div>{obj.name}</div>})}
+//                 </People>
+//                 {/* <Image imageSrc={data.url}/>
+//                 <Title title={data.title}/>
+//                 <Date date={data.date}/>
+//                 <Explanation exp={data.explanation}/>
+//                 <Copyright copyright={data.copyright}/> */}
+//             </div>
+//         )
+
+// }
+
+// export default Data; 
+
+
